27e222da82ad76d847843596113f32523bb7104a
Abdulaziz20102901 <rayimovazinnura1@gmail.com>
2026-04-04 17:33:45 +0500

    whitepace

---
+++ main.js
@@ -0,0 +1,48 @@
+// 1.
+// Kvadrat tomoni 5. Yuzini chiqaring.
+console.log("1 masale:",a=5*5);
+
+// 👉 (faqat console.log(5 * 5) kabi)
+console.log("2 masala:",5*5);
+
+
+// 2.
+// Kvadrat tomoni 8. Perimetrini chiqaring.
+
+console.log("3 masala:",8+8*2);
+
+
+// 3.
+// To‘g‘ri to‘rtburchak: 4 va 6.
+// Yuzini chiqaring.
+
+console.log("4 masala:",4*6);
+
+
+// 4.
+// To‘g‘ri to‘rtburchak: 3 va 7.
+// Perimetrini chiqaring.
+
+console.log(3+7*2);
+
+
+// 5.
+// Doira radiusi 3.
+// Yuzini chiqaring (3.14 ishlating).
+
+// 🟡 Biroz o‘ylash kerak
+
+// 6.
+// 10 sonining kvadratini chiqaring.
+
+// 7.
+// 4 sonining kubini chiqaring.
+
+// 8.
+// 2 + 3 + 5 yig‘indisini chiqaring.
+
+// 9.
+// 10 va 20 ning o‘rtacha qiymatini chiqaring.
+
+// 10.
+// 120 sekundni minutga aylantirib chiqaring.
